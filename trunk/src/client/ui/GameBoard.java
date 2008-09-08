95e0058810750f8ee3b3c3273c222fc45c0242f7
zippanova <zippanova@147f1a93-0656-0410-beda-bdfeebcbccfd>
2008-09-08 23:12:42 +0000

    Testing commit with ssh tunneling.

---
+++ trunk/src/client/ui/GameBoard.java
@@ -0,0 +1,23 @@
+package client.ui;
+
+import java.io.Serializable;
+
+public class GameBoard implements Serializable{
+	
+	public static final int WIDTH = 10;
+	public static final int HEIGHT = 10;
+	
+	private char [][] board;
+	
+	public GameBoard () {
+		board = new char[WIDTH][HEIGHT];
+	}
+	
+	/*public char getLocation (int x, int y) throws IndexOutOfBoundsException {
+		if (x >= WIDTH || y >= HEIGHT)
+			throw new IndexOutOfBoundsException();
+		
+	}*/
+	
+}
+
